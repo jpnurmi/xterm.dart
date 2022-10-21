b4514630a49ef17e80ff272003d6f05033e139a3
J-P Nurmi <jpnurmi@gmail.com>
2022-10-21 15:35:01 +0200

    Sync ShortcutManager's shortcuts in didUpdateWidget

--- lib/src/terminal_view.dart
+++ lib/src/terminal_view.dart
@@ -184,6 +184,7 @@ class TerminalViewState extends State<TerminalView> {
       }
       _scrollController = widget.scrollController ?? ScrollController();
     }
+    _shortcutManager.shortcuts = widget.shortcuts ?? defaultTerminalShortcuts;
     super.didUpdateWidget(oldWidget);
   }
 
